3b2f5409c354c9d8be26f302c23343029ea28b46
yerim <na90303@naver.com>
2026-05-22 10:56:11 +0900

    feat: SecurityConfig 기본 설정 추가

---
+++ src/main/java/com/gnjhh/lxp_2nd/global/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.gnjhh.lxp_2nd.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/auth/login", "/auth/signup", "/courses", "/courses/**")
+                            .permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+}

ba0657fd897b1b08da50a34bfe8c89c6a34c6991
yerim <na90303@naver.com>
2026-05-23 14:49:42 +0900

    feat: MemberRepository loginId 조회 메서드 추가

--- src/main/java/com/gnjhh/lxp_2nd/member/MemberRepository.java
+++ src/main/java/com/gnjhh/lxp_2nd/member/MemberRepository.java
@@ -1,6 +1,7 @@
 package com.gnjhh.lxp_2nd.member;
 
 import com.gnjhh.lxp_2nd.member.domain.entity.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
@@ -8,4 +9,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByLoginId(String loginId);
 
     boolean existsByNickname(String nickname);
+
+    Optional<Member> findByLoginId(String loginId);
 }

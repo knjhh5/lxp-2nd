3750ba036bd43cbed172a82dd3a743acd555a370
sohee <sohee5997@naver.com>
2026-05-23 20:49:55 +0900

    feat: 어드민 강의 목록 조회 Service 로직 구현

--- src/main/java/com/gnjhh/lxp_2nd/admin/AdminService.java
+++ src/main/java/com/gnjhh/lxp_2nd/admin/AdminService.java
@@ -1,7 +1,36 @@
 package com.gnjhh.lxp_2nd.admin;
 
+import com.gnjhh.lxp_2nd.course.CourseRepository;
+import com.gnjhh.lxp_2nd.course.domain.vo.Status;
+import com.gnjhh.lxp_2nd.course.dto.CourseAdminListItemResponseDto;
+import com.gnjhh.lxp_2nd.course.dto.CourseAdminListResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AdminService {
+
+    private final CourseRepository courseRepository;
+
+    public AdminService(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    @Transactional
+    public Page<CourseAdminListItemResponseDto> getCourseListForAdmin(Status status, int page){
+        Pageable pageable = PageRequest.of(page - 1, 10);
+        Page<CourseAdminListResponseDto> courses = courseRepository.findCoursesWithEnrollmentCount(status, pageable);
+
+        return courses.map(dto -> {
+            if (dto.getStatus() == Status.PUBLIC) {
+                Integer remainingSeats = Math.max(0, dto.getCapacity() - dto.getActiveCount().intValue());
+                return new CourseAdminListItemResponseDto(dto, dto.getActiveCount(), dto.getCanceledCount(), remainingSeats);
+            } else {
+                return new CourseAdminListItemResponseDto(dto, null, null, null);
+            }
+        });
+    }
 }

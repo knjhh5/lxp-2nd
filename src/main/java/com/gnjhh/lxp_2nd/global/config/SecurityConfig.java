86ca594913c5bae97814d0cdea78074e1beee0e5
한지오 <3670go@gmail.com>
2026-05-24 13:47:00 +0900

    fix: 정적 리소스 보안 설정 컴파일 오류 수정

--- src/main/java/com/gnjhh/lxp_2nd/global/config/SecurityConfig.java
+++ src/main/java/com/gnjhh/lxp_2nd/global/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.gnjhh.lxp_2nd.global.config;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -22,7 +21,7 @@ public class SecurityConfig {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return web -> web.ignoring()
-                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+                .requestMatchers("/css/**", "/js/**", "/images/**", "/webjars/**", "/favicon.ico");
     }
 
     @Bean

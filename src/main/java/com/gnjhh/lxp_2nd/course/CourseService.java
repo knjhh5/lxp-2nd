634d9fd49e53a6943c2839b8c21d3b586c1424c9
ssggii <haan7525@gmail.com>
2026-05-25 13:24:32 +0900

    refactor: NotFoundException 공통 예외 처리
    리소스를 찾을 수 없는 404 에러에 대하여 공통 예외처리를 추가했습니다.
    
    확장성을 위해 NotFoundException 클래스를 상위로 만들고, 도메인별 NotFoundException이 상위 NotFoundException을 상속받도록 구현했습니다. GlobalExceptionHandler에서는 상위 NotFoundException 타입 예외가 발생하면 공통으로 error/404 페이지를 반환합니다.

--- src/main/java/com/gnjhh/lxp_2nd/course/CourseService.java
+++ src/main/java/com/gnjhh/lxp_2nd/course/CourseService.java
@@ -4,6 +4,7 @@ import com.gnjhh.lxp_2nd.content.ContentRepository;
 import com.gnjhh.lxp_2nd.content.domain.entity.Content;
 import com.gnjhh.lxp_2nd.course.domain.entity.Course;
 import com.gnjhh.lxp_2nd.course.dto.CourseDetailResponse;
+import com.gnjhh.lxp_2nd.global.exception.CourseNotFoundException;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,11 +25,11 @@ public class CourseService {
     public CourseDetailResponse getCourseDetail(Long courseId) {
         // 강의 조회
         Course course = courseRepository.findById(courseId)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 강의입니다"));
+                .orElseThrow(CourseNotFoundException::new);
 
         // 해당 강의의 콘텐츠 목록 조회
         List<Content> contents = contentRepository.findByCourseIdOrderByOrderIndexAsc(courseId);
-        
+
         return CourseDetailResponse.of(course, contents);
     }
 }
